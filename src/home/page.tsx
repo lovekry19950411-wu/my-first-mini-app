6bef6328be8eb1e89136d2640dcfd002762f4fe5
lovekry19950411-wu <lovekry19950411@gmail.com>
2026-04-26 12:59:40 +0800

    clean home page, remove TipBox

--- src/home/page.tsx
+++ src/home/page.tsx
@@ -1,10 +1,8 @@
-import TipBox from "../components/TipBox";
-
 export default function HomePage() {
   return (
-    <div>
-      <h1>歡迎！</h1>
-      <TipBox /> {/* ← 這一行是關鍵！ */}
-    </div>
+    <main className="flex flex-col items-center justify-center min-h-screen p-4">
+      <h1 className="text-2xl font-bold">每日籤詩</h1>
+      <p className="mt-4 text-gray-600">登入後即可抽籤</p>
+    </main>
   );
 }

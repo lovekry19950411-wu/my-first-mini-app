0d5d830163861d47000572893796a831ec806812
lovekry19950411-wu <lovekry19950411@gmail.com>
2026-04-30 20:27:41 +0800

    fix: layout path and provider tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 // src/app/layout.tsx
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import { WorldAppProvider } from "@/providers/WorldAppProviders";
 
 const inter = Inter({ subsets: ["latin"] });

203fcd47d836d546912e7a64d662ce4edf9695d0
TWSPWUZH <lovekry19950411@gmail.com>
2026-05-12 13:02:56 +0800

    fix(official): middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,8 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(_request: NextRequest) {
+export function middleware(_req: NextRequest) {
   return NextResponse.next();
 }
 
-export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-};
+export const config = { matcher: [] };

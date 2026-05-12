d79b82bb2accaa8a444853746737ad2f1a9371ac
TWSPWUZH <lovekry19950411@gmail.com>
2026-05-12 13:13:39 +0800

    fix: middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
 export function middleware(_req: NextRequest) {
   return NextResponse.next();
